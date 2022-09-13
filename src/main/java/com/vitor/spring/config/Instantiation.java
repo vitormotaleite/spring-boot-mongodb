39395a47421507c5509d4863e7525d5948c77d43
vitormotaleite <vitor.mota.diniz@ccc.ufcg.edu.br>
2022-09-13 12:51:30 -0300

    post entidade

--- src/main/java/com/vitor/spring/config/Instantiation.java
+++ src/main/java/com/vitor/spring/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.vitor.spring.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.vitor.spring.domain.Post;
 import com.vitor.spring.domain.User;
+import com.vitor.spring.domain.repository.PostRepository;
 import com.vitor.spring.domain.repository.UserRepository;
 
 @Configuration
@@ -14,11 +18,18 @@ public class Instantiation implements CommandLineRunner {
 	
 	@Autowired
 	UserRepository userRepository;
+	
+	@Autowired
+	PostRepository postRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		this.userRepository.deleteAll();
+		this.postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
@@ -26,6 +37,11 @@ public class Instantiation implements CommandLineRunner {
 		
 		this.userRepository.saveAll(Arrays.asList(maria,alex,bob));
 		
+		Post post1 = new Post(null,sdf.parse("21/08/2018"),"partiu viagem!","vou viajar para sao paulo, abracos!",maria);
+		Post post2 = new Post(null,sdf.parse("23/08/2018"),"bom dia","acordei feliz hoje",maria);
+		
+		this.postRepository.saveAll(Arrays.asList(post1,post2));
+
 	}
 
 }
