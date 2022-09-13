009a57f0e8f15e658f7214243400bb872c60ffc4
vitormotaleite <vitor.mota.diniz@ccc.ufcg.edu.br>
2022-09-13 13:08:25 -0300

    retornando posts

--- src/main/java/com/vitor/spring/resources/UserResource.java
+++ src/main/java/com/vitor/spring/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.vitor.spring.domain.Post;
 import com.vitor.spring.domain.User;
 import com.vitor.spring.dto.UserDTO;
 import com.vitor.spring.services.UserService;
@@ -58,5 +59,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
